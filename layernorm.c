671cfc18cd8b031e43f1e3cb022347e3dcb66944
sherlock <514160797@qq.com>
2024-08-18 22:02:24 +0800

    clang format

--- layernorm.c
+++ layernorm.c
@@ -5,14 +5,22 @@
 
 const float EPS = 1e-5f;
 
-void layernorm_forward(float *inp, float *w, float *bias, float *mean,
-                       float *rstd, float *out, int B, int T, int C) {
+void layernorm_forward(
+    float* inp,
+    float* w,
+    float* bias,
+    float* mean,
+    float* rstd,
+    float* out,
+    int B,
+    int T,
+    int C) {
   for (int b = 0; b < B; ++b) {
     for (int t = 0; t < T; ++t) {
       float m = 0.0f;
 
       // seek to the input position inp[b,t,:]
-      float *x = inp + b * T * C + t * C;
+      float* x = inp + b * T * C + t * C;
       for (int i = 0; i < C; ++i) {
         m += x[i]; // # B,T,1
       }
@@ -27,7 +35,7 @@ void layernorm_forward(float *inp, float *w, float *bias, float *mean,
       float rstd_v = pow(var + EPS, -0.5);
 
       // seek to the output position out[b,t:]
-      float *out_p = out + b * T * C + t * C;
+      float* out_p = out + b * T * C + t * C;
       for (int i = 0; i < C; ++i) {
         out_p[i] = (x[i] - m) * rstd_v * w[i] + bias[i];
       }
@@ -38,16 +46,25 @@ void layernorm_forward(float *inp, float *w, float *bias, float *mean,
   }
 }
 
-void layernorm_backward(float *dout, float *dw, float *db, float *dx,
-                        float *inp, float *w, float *bias, float *mean,
-                        float *rstd, int B, int T, int C) {
-
+void layernorm_backward(
+    float* dout,
+    float* dw,
+    float* db,
+    float* dx,
+    float* inp,
+    float* w,
+    float* bias,
+    float* mean,
+    float* rstd,
+    int B,
+    int T,
+    int C) {
   for (int b = 0; b < B; ++b) {
     for (int t = 0; t < T; ++t) {
       // seek to the position [b,t,:]
-      float *x = inp + b * T * C + t * C;
-      float *dout_p = dout + b * T * C + t * C;
-      float *dx_p = dx + b * T * C + t * C;
+      float* x = inp + b * T * C + t * C;
+      float* dout_p = dout + b * T * C + t * C;
+      float* dx_p = dx + b * T * C + t * C;
 
       float m = mean[b * T + t];
       float v = rstd[b * T + t];
@@ -77,7 +94,7 @@ void layernorm_backward(float *dout, float *dw, float *db, float *dx,
 }
 
 // poor man's tensor checker
-int check_tensor(float *ref, float *pred, int size, const char *label) {
+int check_tensor(float* ref, float* pred, int size, const char* label) {
   printf("check %s\n", label);
   for (int i = 0; i < size; ++i) {
     if (fabs(ref[i] - pred[i]) > 1e-4) {
@@ -90,32 +107,31 @@ int check_tensor(float *ref, float *pred, int size, const char *label) {
 }
 
 int main() {
-
   int B = 2; // batch
   int T = 3;
   int C = 4;
 
-  float *x = (float *)malloc(B * T * C * sizeof(float));
-  float *w = (float *)malloc(C * sizeof(float));
-  float *b = (float *)malloc(C * sizeof(float));
-  float *out = (float *)malloc(B * T * C * sizeof(float));
-  float *dout = (float *)malloc(B * T * C * sizeof(float));
-  float *mean = (float *)malloc(B * T * sizeof(float));
-  float *rstd = (float *)malloc(B * T * sizeof(float));
-  float *dw = (float *)malloc(C * sizeof(float));
-  float *db = (float *)malloc(C * sizeof(float));
-  float *dx = (float *)malloc(B * T * C * sizeof(float));
+  float* x = (float*)malloc(B * T * C * sizeof(float));
+  float* w = (float*)malloc(C * sizeof(float));
+  float* b = (float*)malloc(C * sizeof(float));
+  float* out = (float*)malloc(B * T * C * sizeof(float));
+  float* dout = (float*)malloc(B * T * C * sizeof(float));
+  float* mean = (float*)malloc(B * T * sizeof(float));
+  float* rstd = (float*)malloc(B * T * sizeof(float));
+  float* dw = (float*)malloc(C * sizeof(float));
+  float* db = (float*)malloc(C * sizeof(float));
+  float* dx = (float*)malloc(B * T * C * sizeof(float));
 
   // reference
-  float *out_ref = (float *)malloc(B * T * C * sizeof(float));
-  float *dx_ref = (float *)malloc(B * T * C * sizeof(float));
-  float *mean_ref = (float *)malloc(B * T * sizeof(float));
-  float *rstd_ref = (float *)malloc(B * T * sizeof(float));
-  float *dw_ref = (float *)malloc(C * sizeof(float));
-  float *db_ref = (float *)malloc(C * sizeof(float));
+  float* out_ref = (float*)malloc(B * T * C * sizeof(float));
+  float* dx_ref = (float*)malloc(B * T * C * sizeof(float));
+  float* mean_ref = (float*)malloc(B * T * sizeof(float));
+  float* rstd_ref = (float*)malloc(B * T * sizeof(float));
+  float* dw_ref = (float*)malloc(C * sizeof(float));
+  float* db_ref = (float*)malloc(C * sizeof(float));
 
   // read data from reference information
-  FILE *fp = fopen("ln.bin", "rb");
+  FILE* fp = fopen("ln.bin", "rb");
   if (fp == NULL) {
     printf("File ln.bin not found!");
     return 1;
